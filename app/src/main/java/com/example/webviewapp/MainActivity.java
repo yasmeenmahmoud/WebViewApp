18d8f1f4407c65e511c78fabeb72e272265e620e
yasmeenmahmoud <yasmeenmahmoud@gmail.com>
2018-09-24 01:38:12 +0200

    Initial commit

---
+++ app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.webviewapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText editText = findViewById(R.id.edittext_go);
+        final WebView webView = findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        Button button = findViewById(R.id.btn_go);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                webView.loadUrl(editText.getText().toString());
+            }
+        });
+    }
+}
